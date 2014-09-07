12c06fa704ca2b6bff22bd6156edba4868ac9b1e
randomBEAR <lorenzo.travagliati@gmail.com>
2014-09-08 01:19:08 +0200

    Added a thread that will manage the incoming connection.

--- cinema_server.c
+++ cinema_server.c
@@ -6,16 +6,36 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 
+#define BACKLOG 10
+#define BUFFER 1024
+
 struct incoming {
 	int socket_descriptor;
 	struct sockaddr_in client_data;
 };
 
+void * thread_response(void * sender_info) {
+	char msg[1024];
+	struct incoming * sender = (struct incoming *)sender_info;
+	int ds = sender->socket_descriptor;
+	printf("Descrittore socket sul thread:%d\n",ds);	
+	while(read(sender->socket_descriptor,msg,BUFFER) > 0) {	
+		printf("SENDER:%s",msg);fflush(stdout);
+	}
+	
+}
+
+
 int function() {
 	int ds_sock;
 	int port = 4444;
+	int length_inc;
+	int ds_acc;
+
+	pthread_t tid;
 	
 	struct sockaddr_in addr,inc;
+	struct incoming conn_data;
 	
 	//definizione socket
 	ds_sock = socket(AF_INET,SOCK_STREAM,0);
@@ -38,11 +58,13 @@ int function() {
 
 	while(1) {
 		length_inc = sizeof(inc);
-		while( (ds_acc = accept(ds_sock,(struct sockaddr *)&inc, &length_inc) ) == -1 ) {
+		while( (ds_acc = accept(ds_sock,(struct sockaddr *)&inc, &length_inc) ) == -1 );
 			printf("CONNECTED: socket %d",ds_acc);
 			conn_data.client_data = inc;
 			conn_data.socket_descriptor = ds_acc;
-		}
+			if ( pthread_create(&tid,NULL,*thread_response,(void *)&conn_data) != 0 ) {
+				perror("Thread Creation error");
+			}
 	}	
 }
 
